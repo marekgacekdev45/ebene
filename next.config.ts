1b5c40e4b0069e3d3eaa184821dfdc43a739b56e
marekgacek45 <marekgacek45@gmail.com>
2025-01-08 11:58:59 +0100

    add next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+		ignoreBuildErrors: true,
+	},
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
+  images: {
+		formats: ['image/avif', 'image/webp'],
+	},
 };
 
 export default nextConfig;
