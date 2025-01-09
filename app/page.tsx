d820c8846326cb3b5aae66eb06f4a1e21b49c32d
marekgacek45 <marekgacek45@gmail.com>
2025-01-09 07:39:36 +0100

    testing other sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 // import Hero from '@/components/home/hero'
-// import About from '@/components/home/about'
-// import Stats from '@/components/home/stats'
-// import Offer from '@/components/home/offer'
+import About from '@/components/home/about'
+import Stats from '@/components/home/stats'
+import Offer from '@/components/home/offer'
 import Features from '@/components/home/features'
-// import CTA from '@/components/home/cta'
+import CTA from '@/components/home/cta'
 // import Contact from '@/components/home/contact'
 
 export default function Home() {
 	return (
 		<>
 			{/* <Hero /> */}
-			{/* <About /> */}
-			{/* <Stats /> */}
-			{/* <Offer /> */}
+			<About />
+			<Stats />
+			<Offer />
 			<Features />
-			{/* <CTA /> */}
+			<CTA />
 			{/* <Contact /> */}
 		</>
 	)
