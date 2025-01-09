f1eeeae4667ca923bd4108b5b60326801d21170b
marekgacek45 <marekgacek45@gmail.com>
2025-01-09 07:30:36 +0100

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // import Hero from '@/components/home/hero'
-import About from '@/components/home/about'
+// import About from '@/components/home/about'
 // import Stats from '@/components/home/stats'
 // import Offer from '@/components/home/offer'
 // import Features from '@/components/home/features'
@@ -10,7 +10,7 @@ export default function Home() {
 	return (
 		<>
 			{/* <Hero /> */}
-			<About />
+			{/* <About /> */}
 			{/* <Stats /> */}
 			{/* <Offer /> */}
 			{/* <Features /> */}
