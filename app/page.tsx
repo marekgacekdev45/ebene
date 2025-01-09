f2eb4d111cb6fcffc1ffc955d617fb9e2861c0b3
marekgacek45 <marekgacek45@gmail.com>
2025-01-09 07:23:23 +0100

    testing app

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 import Hero from '@/components/home/hero'
-import About from '@/components/home/about'
-import Stats from '@/components/home/stats'
-import Offer from '@/components/home/offer'
-import Features from '@/components/home/features'
-import CTA from '@/components/home/cta'
-import Contact from '@/components/home/contact'
+// import About from '@/components/home/about'
+// import Stats from '@/components/home/stats'
+// import Offer from '@/components/home/offer'
+// import Features from '@/components/home/features'
+// import CTA from '@/components/home/cta'
+// import Contact from '@/components/home/contact'
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
-			<About />
+			{/* <About />
 			<Stats />
 			<Offer />
 			<Features />
 			<CTA />
-			<Contact />
+			<Contact /> */}
 		</>
 	)
 }
