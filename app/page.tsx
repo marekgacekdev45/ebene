a4635aa8d6d8a9a983fa220e2508c08b249169b6
marekgacek45 <marekgacek45@gmail.com>
2025-01-09 07:41:45 +0100

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
 	return (
 		<>
 			{/* <Hero /> */}
-			<About />
-			<Stats />
-			<Offer />
+			{/* <About /> */}
+			{/* <Stats /> */}
+			{/* <Offer /> */}
 			<Features />
 			<CTA />
 			{/* <Contact /> */}
